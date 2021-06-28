38304abd8094aa9d530a0cb9b7a62a3b2c2b7532
SushiMan <45275055+SushiMan-o3@users.noreply.github.com>
2021-06-28 11:34:06 -0400

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,31 +1,27 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.image import Image
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-Window.size = (1000,700)
+
+Window.size = (1000, 700)
+
 
 class Chat(App):
     def build(self):
         self.icon = 'Assets/chat-logo.ico'
         return ConnectPage()
 
-class ConnectPage(Widget):
+
+class ConnectPage(Screen):
     pass
- 
-class Home(Widget):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.img = Image(source='Assets/background.jpg',
-            size_hint=(1000, 700),
-        )
-        self.add_widget(self.img)
+
+
+class Home(Screen):
+    pass
+
 
 if __name__ == "__main__":
     Chat().run()
